ddf3294e22e7c016080ddbca389054f0ce4f21f7
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2021-05-11 23:35:09 +0200

    fix incorrect link

--- src/pages/projects/emojiaquarium.js
+++ src/pages/projects/emojiaquarium.js
@@ -12,7 +12,7 @@ export default function emojiAquarium() {
       desc="A virtual aquarium with emoji sea creatures, made with vanilla JS and Canvas."
       bottomImg={emojiaquarium2}
       bottomImgAlt="Screenshot of Emoji Aquarium."
-      linkURL="https://agneskalstrom.com/emoji-aquarium/"
+      linkURL="https://agneskalstrom.github.io/emoji-aquarium/"
       linkText="See demo"
     />
   )
