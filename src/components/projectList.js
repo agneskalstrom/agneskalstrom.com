cbfc24216dbab26e932ac55ac0434eb93927b8eb
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2022-01-07 11:20:06 +0100

    clean up

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -1,12 +1,5 @@
-import pomodoro from "../images/pomodoro.jpg"
 import recipe from "../images/recipe-app.jpg"
-import gifMuseum from "../images/gifmuseum.jpg"
-import plantBud from "../images/plantbud.jpg"
 import creativeChase from "../images/creativechase.svg"
-import magazine from "../images/magazine.jpg"
-import emojiAquarium from "../images/emojiaquarium.jpg"
-import bobRoss from "../images/bobross.jpg"
-import pleasantville from "../images/pleasantville.jpg"
 import digisera from "../images/digisera-main.svg"
 
 const allProjects = [
@@ -31,48 +24,6 @@ const allProjects = [
     link: "/work/creative-chase/",
     alt: "Creative Chase",
   },
-  /*{
-    id: 3,
-    title: "museum-of-gifs",
-    img: gifMuseum,
-    link: "/work/gifmuseum/",
-    alt: "Museum of GIFs",
-  },
-  {
-    id: 4,
-    title: "timeless-wisdom",
-    img: bobRoss,
-    link: "/work/timelesswisdom/",
-    alt: "Bob Ross Timeless Wisdom",
-  },
-  {
-    id: 5,
-    title: "postcards-from-pleasantville",
-    img: pleasantville,
-    link: "/work/pleasantville/",
-    alt: "Postcards from Pleasantville",
-  },
-  {
-    id: 6,
-    title: "fictional-magazine",
-    img: magazine,
-    link: "/work/fictionalmagazine/",
-    alt: "Fictional magazine spread",
-  },
-  {
-    id: 7,
-    title: "plantbud",
-    img: plantBud,
-    link: "/work/plantbud/",
-    alt: "PlantBud",
-  },
-  {
-    id: 9,
-    title: "emoji-aquarium",
-    img: emojiAquarium,
-    link: "/work/emojiaquarium/",
-    alt: "Emoji Aquarium",
-  },*/
 ]
 
 export default allProjects
