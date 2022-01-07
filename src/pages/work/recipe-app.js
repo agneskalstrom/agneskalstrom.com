570249b5257aaf3fd6e976b2ccecdf3c315ff4cd
Agnes Kalström <62034881+agneskalstrom@users.noreply.github.com>
2022-01-07 14:30:00 +0100

    update desc

--- src/pages/work/recipe-app.js
+++ src/pages/work/recipe-app.js
@@ -12,7 +12,7 @@ export default function Digisera() {
         case="Designing and building a recipe app for personal use."
         role="Designer and developer."
         problem="My partner and I often feel uninspired when it comes to cooking, but the fact that we have different dietary requirements makes it difficult for us to use most recipe apps available on the market."
-        approach="The app's main purpose is to provide assistance when inspiration is running low, and so a randomized recipe is instantly presented instead of the user having to search for certain dishes or ingredients – since deciding what to search for can feel like as much of an effort as deciding what to eat."
+        approach="As the app's main purpose is to provide assistance when inspiration is running low, a randomized recipe is instantly presented instead of the user having to search for certain dishes or ingredients – since deciding what to search for can feel like as much of an effort as deciding what to eat."
         solution="A super basic app with a list of ingredients, instructions and a button to generate a new recipe."
         tools="Figma, React, Netlify"
         linkPrefix="App:"
